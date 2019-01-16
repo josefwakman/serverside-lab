59aa96946e71bcd95004fc7b2c132168735231a0
josefwakman <josef.wakman@gmail.com>
2019-01-16 15:22:36 +0100

    add search

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const bp = require('body-parser')
 
 const app = express()
+
+app.use(bp.urlencoded({
+    extended: false
+}))
 
 app.engine('hbs', expressHandlebars({
     defaultLayout: 'main',
     extname: '.hbs'
 }))
 
+// --- GET ----
+
 app.get('/', function(request, response) {
     response.render("home.hbs")
 })
 
-app.get('/', function(request, response) {
+app.get('/about', function(request, response) {
     response.render("about.hbs")
 })
+
+app.get('/search', function(request, response) {
+    response.render("search.hbs")
+})
+
+// --- POST ----
+app.post('/s', function(req, res) {
+    const city = req.body.city
+})
+
 
 app.listen(8080)
 
