98659b9bbe87e9f9a568ade4b92210e58171c42e
josefwakman <josef.wakman@gmail.com>
2019-01-18 13:20:57 +0100

    post-mathtest

--- main.js
+++ main.js
@@ -27,9 +27,53 @@ app.get('/search', function(request, response) {
     response.render("search.hbs")
 })
 
+app.get('/post-mathtest', function(request, response) {
+
+    const firstNumber = Math.floor(Math.random() * 10)
+    const secondNumber = Math.floor(Math.random() * 10)
+    let operand = ""
+    let answer = ""
+
+    switch(Math.ceil(Math.random() * 4)) {
+        case 1:
+            operand = "+"
+            answer = firstNumber + secondNumber
+            break
+        
+        case 2:
+            operand = "-"
+            answer = firstNumber - secondNumber
+            break
+
+        case 3:
+            operand = "*"
+            answer = firstNumber * secondNumber
+            break
+
+        case 4:
+            operand = "/"
+            answer = firstNumber / secondNumber
+            break
+    }
+
+    const context = {
+        firstNumber: firstNumber,
+        secondNumber: secondNumber,
+        operand: operand,
+        answer: answer
+    }
+    response.render("post-mathtest.hbs", context)
+})
+
 // --- POST ----
 app.post('/s', function(req, res) {
-    const city = req.body.city
+    const context = req.body
+    res.render("search.hbs", context)
+})
+
+app.post('/post-mathtest', function(request, response) {
+
+    res.render("post-mathtest", context)
 })
 
 
