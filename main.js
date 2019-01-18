6ce04f2393321f963076626554a9b0e7323a0714
josefwakman <josef.wakman@gmail.com>
2019-01-18 14:49:05 +0100

    implement math pages

--- main.js
+++ main.js
@@ -27,7 +27,102 @@ app.get('/search', function(request, response) {
     response.render("search.hbs")
 })
 
-app.get('/post-mathtest', function(request, response) {
+let latestContext = false
+
+app.get('/math-test', function(request, response) {
+
+    const firstNumber = Math.floor(Math.random() * 10)
+    const secondNumber = Math.floor(Math.random() * 10)
+    let operand = ""
+    let answer = ""
+
+    switch(Math.ceil(Math.random() * 4)) {
+        case 1:
+            operand = "+"
+            answer = firstNumber + secondNumber
+            break
+        
+        case 2:
+            operand = "-"
+            answer = firstNumber - secondNumber
+            break
+
+        case 3:
+            operand = "*"
+            answer = firstNumber * secondNumber
+            break
+
+        case 4:
+            operand = "/"
+            answer = firstNumber / secondNumber
+            break
+    }
+
+    const answeredCorrectly = (latestContext.answer == answer)
+
+    const context = {
+        firstNumber: firstNumber,
+        secondNumber: secondNumber,
+        operand: operand,
+        answer: answer,
+        answeredCorrectly: answeredCorrectly,
+        latestContext: false
+    }
+    latestContext = context
+
+    response.render("math-test.hbs", context)
+})
+
+app.get('/calculator', function(request, response) {
+    const context = {
+        answered: false
+    }
+
+    console.log(request.query)
+    
+    
+    if (Object.keys(request.query).length) {
+        context.answered = true
+
+        switch(request.query.operand) {
+            case "+":
+                context.operand = "+"
+                context.answer = parseFloat(request.query.firstNumber) + parseFloat(request.query.secondNumber)
+                break
+            case "-":
+                context.operand = "-"
+                context.answer = parseFloat(request.query.firstNumber) - parseFloat(request.query.secondNumber)
+                break
+            case "*":
+                context.operand = "*"
+                context.answer = parseFloat(request.query.firstNumber) * parseFloat(request.query.secondNumber)
+                break
+            case "/":
+                context.operand = "/"
+                context.answer = parseFloat(request.query.firstNumber) / parseFloat(request.query.secondNumber)
+                break
+            default:
+                context.errorMessage = "Invalid operand"
+        }
+        if (!context.errorMessage) {
+            context.firstNumber = request.query.firstNumber
+            context.secondNumber = request.query.secondNumber
+        }
+    }
+
+    console.log(context)
+    
+    response.render("calculator.hbs", context)
+})
+
+// --- POST ----
+app.post('/s', function(request, response) {
+    const context = req.body
+    response.render("search.hbs", context)
+})
+
+app.post('/math-test', function(request, response) {
+    const answeredCorrectly = latestContext.answer == request.body.answer
 
     const firstNumber = Math.floor(Math.random() * 10)
     const secondNumber = Math.floor(Math.random() * 10)
@@ -60,20 +155,14 @@ app.get('/post-mathtest', function(request, response) {
         firstNumber: firstNumber,
         secondNumber: secondNumber,
         operand: operand,
-        answer: answer
+        answer: answer,
+        answeredCorrectly: answeredCorrectly,
+        latestContext: latestContext
     }
-    response.render("post-mathtest.hbs", context)
-})
 
-// --- POST ----
-app.post('/s', function(req, res) {
-    const context = req.body
-    res.render("search.hbs", context)
-})
-
-app.post('/post-mathtest', function(request, response) {
-
-    res.render("post-mathtest", context)
+    latestContext = context
+    
+    response.render("math-test.hbs", context)
 })
 
 
